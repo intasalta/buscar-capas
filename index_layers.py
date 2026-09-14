9b56cf3fd35bf01e60ac8b80abb30d88c2fe0779
intasalta <rrnn.eeasalta@gmail.com>
2026-09-14 16:18:52 -0300

    Add files via upload

--- index_layers.py
+++ index_layers.py
@@ -8,6 +8,12 @@ import os
 import sys
 import logging
 from datetime import datetime
+
+if sys.stdout and hasattr(sys.stdout, "reconfigure"):
+    sys.stdout.reconfigure(encoding="utf-8")
+if sys.stderr and hasattr(sys.stderr, "reconfigure"):
+    sys.stderr.reconfigure(encoding="utf-8")
+
 from wms.indexer import index_all_wms
 
 logging.basicConfig(
